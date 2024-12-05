004ce65f8389488a93bd1c2fe4c0c52be09dc542
Arthur Lima <artglima.dev@gmail.com>
2024-12-05 12:09:51 -0300

    feat: testing compatible Wallet

--- src/test/java/com/picpaysimple/repositories/WalletRepositoryTest.java
+++ src/test/java/com/picpaysimple/repositories/WalletRepositoryTest.java
@@ -1,8 +1,19 @@
 package com.picpaysimple.repositories;
 
+import com.picpaysimple.dtos.WalletDTO;
+import com.picpaysimple.entities.wallet.Wallet;
+import com.picpaysimple.entities.wallet.WalletType;
+import jakarta.persistence.EntityManager;
+import org.hibernate.usertype.UserType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +21,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class WalletRepositoryTest {
 
+    @Autowired
+    WalletRepository walletRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
     @Test
-    void findWalletByDocument() {
+    @DisplayName("Should get Wallet successfully from DB")
+    void findWalletByDocumentSuccess() {
+        String document = "99999999901";
+        WalletDTO data = new WalletDTO("João da Silva", document, new BigDecimal(10), "teste@gmail.com", "4444", WalletType.COMMON);
+        this.createWallet(data);
+
+        Optional<Wallet> foundedWallet = this.walletRepository.findWalletByDocument(document);
+
+        assertThat(foundedWallet.isPresent()).isTrue();
+    }
+
+    private Wallet createWallet(WalletDTO data) {
+        Wallet newWallet = new Wallet(data);
+        this.entityManager.persist(newWallet);
+        return newWallet;
     }
 }
